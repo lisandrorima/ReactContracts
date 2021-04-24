cd2a25a747b0af403e541c56cae048af8b82e898
Facu <64644557+fsayago@users.noreply.github.com>
2021-04-24 19:06:58 -0300

    Armando portada

--- client/src/components/HomeComp.js
+++ client/src/components/HomeComp.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+import { NavLink } from 'react-router-dom'
 
 export class HomeComp extends Component {
     render() {
         return (
             <div>
-                Esto es Home
+            <div className="hero-image">
+                            <div className="hero-text">
+              <h1>Encuentra tu hogar</h1>
+              <p>Vive tu vida</p>
+              <button><NavLink to="/home">Buscar Propiedades</NavLink></button>
             </div>
+          </div> 
+          </div>
         )
     }
 }
